eabdc2385e271bee54299824a4c6e566c8fe1b66
Kaji Nasiruddin Ahmed <nasir.ciem.it@gmail.com>
2022-10-02 21:15:37 +0530

    First Commit

---
+++ kiteconnect/__version__.py
@@ -0,0 +1,7 @@
+__title__ = "kiteconnect"
+__description__ = "The unofficial Python client for the Kite Connect trading API"
+__url__ = "https://kite.trade"
+__download_url__ = "https://github.com/nasir101/kiteconnect"
+__version__ = "4.1.0"
+__author__ = "Kaji Nasiruddin Ahmed"
+__author_email__ = "nasir.ciem.it@gmail.com"
